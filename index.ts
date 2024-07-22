62b8f85035dd77f0110c3b63c36302bd4eea99b3
Abraham Guo <aguo@precisionplanting.com>
2024-07-22 07:03:52 -0500

    finish

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { parseForESLint } from '@typescript-eslint/parser';
 import type { TSESTree } from '@typescript-eslint/types';
-
 import { typeMatchesSpecifier } from '@typescript-eslint/type-utils';
 
 const { ast, services } = parseForESLint('type Test = URL;', {
@@ -8,24 +7,28 @@ const { ast, services } = parseForESLint('type Test = URL;', {
 	project: './tsconfig.json',
 	filePath: `${import.meta.dirname}/index.ts`
 });
+
 console.log(
-	(
-		[
-			{ from: 'package', name: 'URL', package: 'node' },
-			{ from: 'lib', name: 'URL' },
-		] as const
-	).map(specifier =>
-		typeMatchesSpecifier(
-			services
-				.program!.getTypeChecker()
-				.getTypeAtLocation(
-					services.esTreeNodeToTSNodeMap.get(
-						(ast.body[ast.body.length - 1] as TSESTree.TSTypeAliasDeclaration)
-							.id,
+	Object.fromEntries(
+		(
+			[
+				{ from: 'package', name: 'URL', package: 'node' },
+				{ from: 'lib', name: 'URL' }
+			] as const
+		).map(specifier => [
+			specifier.from,
+			typeMatchesSpecifier(
+				services
+					.program!.getTypeChecker()
+					.getTypeAtLocation(
+						services.esTreeNodeToTSNodeMap.get(
+							(ast.body[ast.body.length - 1] as TSESTree.TSTypeAliasDeclaration)
+								.id
+						)
 					),
-				),
-			specifier,
-			services.program!,
-		),
-	),
+				specifier,
+				services.program!
+			)
+		])
+	)
 );
