eae1c5f2cbcbee00987c1d2f9af94a7082498988
Abraham Guo <aguo@precisionplanting.com>
2024-07-22 08:50:07 -0500

    make generic

--- index.ts
+++ index.ts
@@ -3,23 +3,24 @@ import type { TSESTree } from '@typescript-eslint/types';
 import { typeMatchesSpecifier } from '@typescript-eslint/type-utils';
 
 const mapToObj = <T>(arr: T[], fn: (item: T) => [string, unknown]) =>
-	Object.fromEntries(arr.map(fn));
+		Object.fromEntries(arr.map(fn)),
+	name = `URL`;
 
 console.log(
 	mapToObj(['tsconfig', 'tsconfig.no-dom'], tsconfig => {
 		// https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/type-utils/tests/TypeOrValueSpecifier.test.ts#L127
-		const { ast, services } = parseForESLint(`type Test = URL;`, {
-			disallowAutomaticSingleRunInference: true,
-			project: `./${tsconfig}.json`,
-			filePath: `${import.meta.dirname}/index.ts`
-		});
-		const program = services.program!;
+		const { ast, services } = parseForESLint(`type Test = ${name};`, {
+				disallowAutomaticSingleRunInference: true,
+				project: `./${tsconfig}.json`,
+				filePath: `${import.meta.dirname}/index.ts`
+			}),
+			program = services.program!;
 		return [
 			tsconfig,
 			mapToObj(
 				[
-					{ from: 'package', name: 'URL', package: 'node' },
-					{ from: 'lib', name: 'URL' }
+					{ from: `package`, name, package: `node` },
+					{ from: `lib`, name }
 				] as const,
 				specifier => [
 					specifier.from,
