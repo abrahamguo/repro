69df5e2dd37318c153e20a08e317093886428359
Abraham Guo <aguo@precisionplanting.com>
2024-07-22 07:24:49 -0500

    simplify

--- index.ts
+++ index.ts
@@ -2,16 +2,8 @@ import { parseForESLint } from '@typescript-eslint/parser';
 import type { TSESTree } from '@typescript-eslint/types';
 import { typeMatchesSpecifier } from '@typescript-eslint/type-utils';
 
-const mapToObj = <T extends string | object>(
-	arr: T[],
-	fn: (item: T) => unknown
-) =>
-	Object.fromEntries(
-		arr.map(item => [
-			typeof item == 'string' ? item : Object.values(item)[0],
-			fn(item)
-		])
-	);
+const mapToObj = <T>(arr: T[], fn: (item: T) => [string, unknown]) =>
+	Object.fromEntries(arr.map(fn));
 
 console.log(
 	mapToObj(['tsconfig', 'tsconfig.no-dom'], tsconfig => {
@@ -21,23 +13,28 @@ console.log(
 			filePath: `${import.meta.dirname}/index.ts`
 		});
 		const program = services.program!;
-		return mapToObj(
-			[
-				{ from: 'package', name: 'URL', package: 'node' },
-				{ from: 'lib', name: 'URL' }
-			] as const,
-			specifier =>
-				typeMatchesSpecifier(
-					program
-						.getTypeChecker()
-						.getTypeAtLocation(
-							services.esTreeNodeToTSNodeMap.get(
-								(ast.body.at(-1) as TSESTree.TSTypeAliasDeclaration).id
-							)
-						),
-					specifier,
-					program
-				)
-		);
+		return [
+			tsconfig,
+			mapToObj(
+				[
+					{ from: 'package', name: 'URL', package: 'node' },
+					{ from: 'lib', name: 'URL' }
+				] as const,
+				specifier => [
+					specifier.from,
+					typeMatchesSpecifier(
+						program
+							.getTypeChecker()
+							.getTypeAtLocation(
+								services.esTreeNodeToTSNodeMap.get(
+									(ast.body.at(-1) as TSESTree.TSTypeAliasDeclaration).id
+								)
+							),
+						specifier,
+						program
+					)
+				]
+			)
+		];
 	})
 );
