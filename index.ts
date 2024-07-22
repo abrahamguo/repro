b26b67a62928b994fb33cfaf8dd9267e8ab12735
Abraham Guo <aguo@precisionplanting.com>
2024-07-22 07:10:52 -0500

    push in lib

--- index.ts
+++ index.ts
@@ -2,33 +2,41 @@ import { parseForESLint } from '@typescript-eslint/parser';
 import type { TSESTree } from '@typescript-eslint/types';
 import { typeMatchesSpecifier } from '@typescript-eslint/type-utils';
 
-const { ast, services } = parseForESLint('type Test = URL;', {
-	disallowAutomaticSingleRunInference: true,
-	project: './tsconfig.json',
-	filePath: `${import.meta.dirname}/index.ts`
-});
-const { program } = services;
+const mapToObj = <T>(arr: T[], fn: (item: T) => unknown) =>
+	Object.fromEntries(
+		arr.map(item => [
+			typeof item == 'string' ? item : JSON.stringify(item),
+			fn(item)
+		])
+	);
 
 console.log(
-	Object.fromEntries(
-		(
-			[
-				{ from: 'package', name: 'URL', package: 'node' },
-				{ from: 'lib', name: 'URL' }
-			] as const
-		).map(specifier => [
-			specifier.from,
-			typeMatchesSpecifier(
-				program
-					.getTypeChecker()
-					.getTypeAtLocation(
-						services.esTreeNodeToTSNodeMap.get(
-							(ast.body.at(-1) as TSESTree.TSTypeAliasDeclaration).id
-						)
-					),
-				specifier,
-				program
-			)
-		])
+	mapToObj([{ lib: ['esnext'] }], lib =>
+		mapToObj([`URL`], type => {
+			const { ast, services } = parseForESLint(`type Test = ${type};`, {
+				disallowAutomaticSingleRunInference: true,
+				project: './tsconfig.json',
+				filePath: `${import.meta.dirname}/index.ts`
+			});
+			const program = services.program!;
+			return mapToObj(
+				[
+					{ from: 'package', name: 'URL', package: 'node' },
+					{ from: 'lib', name: 'URL' }
+				] as const,
+				specifier =>
+					typeMatchesSpecifier(
+						program
+							.getTypeChecker()
+							.getTypeAtLocation(
+								services.esTreeNodeToTSNodeMap.get(
+									(ast.body.at(-1) as TSESTree.TSTypeAliasDeclaration).id
+								)
+							),
+						specifier,
+						program
+					)
+			);
+		})
 	)
 );
