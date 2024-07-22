bca784ef1b76460f72bd5f06451885be81f14631
Abraham Guo <aguo@precisionplanting.com>
2024-07-22 07:20:02 -0500

    add two types

--- index.ts
+++ index.ts
@@ -2,20 +2,23 @@ import { parseForESLint } from '@typescript-eslint/parser';
 import type { TSESTree } from '@typescript-eslint/types';
 import { typeMatchesSpecifier } from '@typescript-eslint/type-utils';
 
-const mapToObj = <T>(arr: T[], fn: (item: T) => unknown) =>
+const mapToObj = <T extends string | object>(
+	arr: T[],
+	fn: (item: T) => unknown
+) =>
 	Object.fromEntries(
 		arr.map(item => [
-			typeof item == 'string' ? item : JSON.stringify(item),
+			typeof item == 'string' ? item : Object.values(item)[0],
 			fn(item)
 		])
 	);
 
 console.log(
-	mapToObj([{ lib: ['esnext'] }], lib =>
-		mapToObj([`URL`], type => {
+	mapToObj(['tsconfig', 'tsconfig.no-dom'], tsconfig =>
+		mapToObj(['URL', 'URLSearchParams'], type => {
 			const { ast, services } = parseForESLint(`type Test = ${type};`, {
 				disallowAutomaticSingleRunInference: true,
-				project: './tsconfig.json',
+				project: `./${tsconfig}.json`,
 				filePath: `${import.meta.dirname}/index.ts`
 			});
 			const program = services.program!;
