03e2062daaeae4e9e0f9a3ee4ad5b9996de40e94
Abraham Guo <aguo@precisionplanting.com>
2024-08-08 13:13:20 -0500

    4.1.1

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log(require(`eslint-plugin-unused-imports`));
--- index.ts
+++
@@ -1,4 +0,0 @@
-import tseslint from 'typescript-eslint';
-import { FlatCompat } from '@eslint/eslintrc';
-
-tseslint.config(...new FlatCompat().config({}));
