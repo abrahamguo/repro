fe203dd0df7cac312151667e23c23302def37798
Abraham Guo <aguo@precisionplanting.com>
2024-10-18 14:34:52 -0500

    WIP

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
 export default { rules: { 'no-lonely-if': 'error' } };
 
 if (true)
-	if (false) {
-	} else {
-		if (false) {
-		}
-	}
+	if (false) {}
+	else { if (false) {} }
 else console.log("This doesn't log. But after running --fix, it does log.");
