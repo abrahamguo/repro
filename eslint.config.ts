1a7312ca88e3c6ae94de021d1d4d85c2f16f05a5
Abraham Guo <aguo@precisionplanting.com>
2024-10-21 08:47:24 -0500

    WIP

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,20 @@
 import tseslint from 'typescript-eslint';
 
-const curly = ['error'];
-tseslint.config({ rules: { curly } });
+const mapObj = <K extends string, V, T>(
+	obj: Record<K, V>,
+	fn: (key: K, val: V, i: number) => T
+) => Object.entries(obj).map((entry, i) => fn(...(entry as [K, V]), i));
+
+tseslint.config(
+	...mapObj(
+		{
+			[`.js,jsx`]: {
+				rules: { 'unicorn/filenameCase': [`error`, { case: `pascalCase` }] }
+			}
+		},
+		(files, overrides) =>
+			[overrides]
+				.flat()
+				.map(override => ({ files: files.split(`,`), ...override }))
+	).flat()
+);
