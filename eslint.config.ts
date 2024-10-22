33504d0186c8c0f64d1109eeb2c31e9d7f267a9a
Abraham Guo <aguo@precisionplanting.com>
2024-10-22 07:37:00 -0500

    WIP

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 const rules = Object.fromEntries(
 	Object.entries({
 		consistentTypeImports: { prefer: 'no-type-imports' },
-		consistentTypeDefinitions: 'type',
+		consistentTypedDefinitions: 'type',
 		noUnusedExpressions: { enforceForJSX: true }
 	}).map(([ruleName, options]) => [ruleName, ['error', options]])
 );
