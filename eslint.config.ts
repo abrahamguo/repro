9cbacac8990d4c505a464c451af42dfe469f9e12
Abraham Guo <aguo@precisionplanting.com>
2024-10-22 07:40:50 -0500

    WIP

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,15 +2,12 @@ import tseslint from 'typescript-eslint';
 
 const rules = Object.fromEntries(
 	Object.entries({
-		arrayCallbackReturn: { allowImplicit: true },
-		curly: 'multi',
-		noConstantCondition: { checkLoops: false },
-		noElseReturn: { allowElseIf: false },
-		noEmpty: { allowEmptyCatch: true },
-		noUnneededTernary: { defaultAssignment: false },
-		objectShorthand: ['always', { avoidExplicitReturnArrows: true }],
-		oneVar: { initialized: 'never', uninitialized: 'always' },
-		requireAwait: []
-	}).map(([ruleName, options]) => [ruleName, ['error', options]])
+		consistentTypeImports: { prefer: 'no-type-imports' },
+		consistentTypeDefinitions: 'type',
+		noUnusedExpressions: { enforceForJSX: true }
+	}).map(([ruleName, options]) => [
+		`@typescript-eslint/${ruleName}`,
+		['error', options]
+	])
 );
 export default tseslint.config({ rules });
