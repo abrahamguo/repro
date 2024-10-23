8737c091e4c66ad8192c4e4181d5b39c72c17a94
Abraham Guo <aguo@precisionplanting.com>
2024-10-22 22:35:25 -0500

    WIP

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import functional from 'eslint-plugin-functional';
-import parser from '@typescript-eslint/parser';
-
-export default {
-	files: ['*.ts'],
-	languageOptions: { parser, parserOptions: { project: true } },
-	plugins: { functional },
-	rules: { 'functional/prefer-tacit': 'error' }
-};
--- index.ts
+++
@@ -1,5 +0,0 @@
-function f(x) {
-	return x + 1;
-}
-
-const foo = [1, 2, 3].map(x => f(x));
