a366abcb4333dd0c43636b1c09ec9e495b97c277
Abraham Guo <aguo@precisionplanting.com>
2024-12-15 16:36:41 -0600

    delete everything

--- main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Heading from "./Heading.tsx";
-import { exampleNode } from "./nodes.ts";
-
-createRoot(document.querySelector(`div`)!).render(
-  <Heading node={exampleNode} />,
-);
--- vite.config.ts
+++
@@ -1,3 +0,0 @@
-import react from "@vitejs/plugin-react";
-
-export default { plugins: [react()] };
