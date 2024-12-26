10f3398899ddebf680f41a1cb61d9db9019bfd3c
Abraham Guo <aguo@precisionplanting.com>
2024-12-26 08:24:59 -0600

    finish

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,1 @@
-import eslintPluginReactCompiler from "eslint-plugin-react-compiler";
-import "./package.json" with { type: "json" };
-
-export default [
-  {
-    plugins: { "react-compiler": eslintPluginReactCompiler },
-  },
-  {
-    files: ["*.{js,ts}"],
-    rules: { "react-compiler/react-compiler": "error" },
-  },
-];
+export default [undefined];
