aa422801da29ba537c5bab4e4a65f1006340ac89
Abraham Guo <aguo@precisionplanting.com>
2025-08-26 07:30:31 -0500

    WIP

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
+import { format } from "prettier";
 import { parseForESLint } from "svelte-eslint-parser";
 
 console.log(
-  parseForESLint(`<script lang='ts'></script><input onchange={e => e} />`),
+  await format(
+    parseForESLint(
+      `<script lang='ts'></script><input onchange={e => {e; let b: number;}} />`,
+      {
+        parser: `@typescript-eslint/parser`,
+      },
+    )._virtualScriptCode,
+    { parser: `typescript` },
+  ),
 );
