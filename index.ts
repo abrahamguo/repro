8540714b56d73a4d24693a4803b25bc1ff74bcc4
Abraham Guo <aguo@precisionplanting.com>
2025-08-26 07:34:05 -0500

    finish

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import { format } from "prettier";
 import { parseForESLint } from "svelte-eslint-parser";
 
 console.log(
-  await format(
-    parseForESLint(
-      `<script lang='ts'></script><input onchange={e => {e; let b: number;}} />`,
-      {
-        parser: `@typescript-eslint/parser`,
-      },
-    )._virtualScriptCode,
-    { parser: `typescript` },
+  Object.fromEntries(
+    [``, `: number`].map((append) => {
+      const statement = `let b${append};`;
+      return [
+        statement,
+        parseForESLint(
+          `<script lang='ts'></script><input onchange={e => {e; ${statement}}} />`,
+          { parser: `@typescript-eslint/parser` },
+        )._virtualScriptCode.trim(),
+      ];
+    }),
   ),
 );
